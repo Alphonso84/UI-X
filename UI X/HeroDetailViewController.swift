1a19c47d231d79ac609d1c2a0fea0217b6cd2781
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2019-01-08 10:20:45 -0800

    building delegate

--- UI X/HeroDetailViewController.swift
+++ UI X/HeroDetailViewController.swift
@@ -10,18 +10,36 @@ import Foundation
 import UIKit
 
 
-class HeroDetailViewController: UIViewController {
+class HeroDetailViewController: UIViewController, ViewControllerDelegate {
     
-    @IBOutlet weak var heroImage: UIImageView!
+    
+    
+    @IBOutlet weak var heroImageView: UIImageView!
+    
     @IBOutlet weak var heroBio: UITextView!
+    
+   
+    
+    func assignImage(_ image: UIImage?) {
+       self.heroImageView.image = image
+        
+    }
+    func assignBio(_ bio: String?) {
+       heroBio.text = bio
+    }
+    
+    func assignName(_ name: String?) {
+        navigationController?.title = name
+    }
     
     
     override func viewWillAppear(_ animated: Bool) {
-        
+       
     }
     
     override func viewDidLoad() {
-        
+        super.viewDidLoad()
+      
     }
     
     
--- UI X/ViewController.swift
+++ UI X/ViewController.swift
@@ -8,9 +8,19 @@
 
 import UIKit
 
-var HeroesArray = [Hero]()
+
+
+protocol ViewControllerDelegate: class{
+    func assignImage(_ image: UIImage?)
+    func assignBio(_ bio: String?)
+    func assignName(_ name: String?)
+}
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    weak var delegate: ViewControllerDelegate?
+    var HeroesArray = [Hero]()
+    @IBOutlet weak var tableView: UITableView!
     
     let Spiderman = Hero(name: "Spider-Man", picture: UIImage(named: "Spiderman")!, bio: "Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider. Adopting the name Spider-Man, Peter hoped to start a career using his new abilities. Taught that with great power comes great responsibility, Spidey has vowed to use his powers to help people.")
     
@@ -20,7 +30,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     let Hulk = Hero(name: "Hulk", picture: UIImage(named: "Hulk")!, bio: "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets. ")
     
-    @IBOutlet weak var tableView: UITableView!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return HeroesArray.count
@@ -30,19 +39,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyCell
         cell.heroImage.image = HeroesArray[indexPath.row].picture
         cell.heroLabel.text = HeroesArray[indexPath.row].name
+       
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let detailViewController = storyboard?.instantiateViewController(withIdentifier: "HeroDetailViewController")
-        
-      // HeroDetailViewController().heroImage.image = HeroesArray[indexPath.row].picture
-     //  HeroDetailViewController().heroBio.text = "Test this is a test this is a test this is a test"
-        
+      
+        delegate?.assignBio(HeroesArray[0].bio)
+        delegate?.assignName(HeroesArray[0].name)
+        delegate?.assignImage(HeroesArray[0].picture)
+    
     }
     
+    
+    
     func populateHeroesArray() {
-        HeroesArray.removeAll()
+   
         HeroesArray.append(Spiderman)
         HeroesArray.append(CaptAmerica)
         HeroesArray.append(Ironman)
@@ -56,7 +68,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        
     }
     
     
